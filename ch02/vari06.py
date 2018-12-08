bcd622d347a4f3e6b824af9b4687860c3285bc16
oyrungi <45708132+oyrungi@users.noreply.github.com>
2018-12-08 17:03:27 +0900

    ch02
    ch02

---
+++ ch02/vari06.py
@@ -0,0 +1,20 @@
+size = input("집의 크기는 얼마로 할까요?")
+size= int(size)
+import turtle
+t= turtle
+t.shape("turtle")
+
+t.forward(size)
+t.rrght(90)
+t.forward(size)
+t.right(90)
+t.forward(size)
+t.right(90)
+t.forward(size)
+
+t.forward(size)
+t.left(60)
+t.forward(100)
+t.left(120)
+t.forward(size)
+
